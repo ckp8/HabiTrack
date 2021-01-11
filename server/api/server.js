dd35e631af3931bd565eb07d8935ed1eb14de09b
Tara <TaraHussain97@gmail.com>
2021-01-11 23:30:39 +0000

    add comments to separate middleware from routes

--- server/api/server.js
+++ server/api/server.js
@@ -1,16 +1,18 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const server = express();
-server.use(cors());
+
+// MIDDLEWARE
+server.use(cors()); // gives access to req.body
 server.use(express.json());
 
-const habitsRoutes = require('./routes/habits')
-const usersRoutes = require('./routes/users')
-server.use('/habits', habitsRoutes)
-server.use('/users', usersRoutes)
+// ROUTES
+const habitsRoutes = require("./routes/habits");
+const usersRoutes = require("./routes/users");
+server.use("/habits", habitsRoutes);
+server.use("/users", usersRoutes);
 
+server.get("/", (req, res) => res.send("Welcome"));
 
-server.get('/', (req, res) => res.send('Welcome'))
-
-module.exports = server
+module.exports = server;
