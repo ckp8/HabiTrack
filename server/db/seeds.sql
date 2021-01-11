d5717c7d5e94a81f45247d4aea08ab4b2a3f40fe
Tara <TaraHussain97@gmail.com>
2021-01-11 18:33:54 +0000

    add email to db

--- server/db/seeds.sql
+++ server/db/seeds.sql
@@ -1,12 +1,20 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS habits;
 
 CREATE TABLE users (
-    id serial PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY NOT NULL,
     username VARCHAR(40) NOT NULL,
-    password VARCHAR(20) NOT NULL
+    email VARCHAR(200) NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    UNIQUE (email)
 );
 
-DROP TABLE IF EXISTS habits;
+INSERT INTO users 
+    (username, email, password)
+VALUES
+    ('test1', 'test1@gmail.com', '$2b$10$hlE6eRv//VgUa7lIREtUbuRvyq8pqCr7Cu3mVtv757M4mcwHMKTT2'),
+    ('test2', 'test2@gmail.com', '$2b$10$sMq2GR7ZERkJ/mEl0v/8sOy4Os0d1kLOkg6abEr0X4.HHYTBYZ8e6')
+;
 
 CREATE TABLE habits (
     id serial PRIMARY KEY,
@@ -16,12 +24,6 @@ CREATE TABLE habits (
     user_id INT
 );
 
-INSERT INTO users (username, password)
-VALUES
-(
-    'Test_User',
-    '12345'
-);
 
 INSERT INTO habits (title, description, completed, user_id)
 VALUES
