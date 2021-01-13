c519a3f9e828f1a233460fda8be78cab2fb89a58
ckp8 <charanjit127@hotmail.com>
2021-01-13 16:34:22 +0000

    added few tests

---
+++ client/src/layout/Header/Header.test.js
@@ -0,0 +1,15 @@
+import Header from '.';
+
+describe('tests for header', () => {
+    let wrapper;
+    beforeEach(() => {
+        wrapper = shallow(<Header />);
+    });
+    test('it exists', () => {
+        expect(wrapper).toExist;
+    });
+    test('it contains an h1', () => {
+        const h1 = wrapper.find('h1');
+        expect(h1.text()).toBe('HabiTrack');
+    });
+});
