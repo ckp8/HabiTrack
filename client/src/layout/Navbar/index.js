4ca1d49e1a426090557cd6ba423029acbb0c9784
Stylianos Thomas <stylianos.thomas@gmail.com>
2021-01-13 11:00:21 +0000

    minor changes

--- client/src/layout/Navbar/index.js
+++ client/src/layout/Navbar/index.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './navbar.css';
 
 class Navbar extends React.Component {
 
-
     render() {
         return (
-            <div>
-
+            <div id="buttonContainer">
+                <button onClick={() => { history.push('/today') }}> Today's Habits </button>
+                <button onClick={() => { history.push('/all') }}> All Habits </button>
+                <button onClick={() => { history.push('/achievements') }}> Achievements </button>
             </div>
         )
     }
