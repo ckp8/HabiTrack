45f1c1a5cb5f71d6f001fd6019f2eadb04b42a22
Tara <TaraHussain97@gmail.com>
2021-01-13 14:21:26 +0000

    add client side UI for register and login pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,88 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { Fragment, useState, useEffect } from "react";
+// import { toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
 
-// import { Landing, Authors, Books, NotFound404 } from './pages';
-// import { NavBar, Footer } from './layout';
+// toast.configure();
 
-import './styles/app.css';
+function App() {
+  const checkAuthenticated = async () => {
+    try {
+      const res = await fetch("http://localhost:3000/auth/verified-user", {
+        method: "GET",
+        headers: { token: localStorage.token },
+      });
 
-const App = () => {
-    return (
-        <div id="App">
-            {/* <NavBar /> */}
+      const parsedResponse = await res.json();
+      parsedResponse === true
+        ? setIsAuthenticated(true)
+        : setIsAuthenticated(false);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
 
-            <main>
-            <Switch>
-                {/* <Route exact path="/" component={Landing} />
-                <Route path="/authors" component={Authors} />
-                <Route path="/books" component={Books} />
-                <Route path="/*" component={NotFound404} /> */}
-            </Switch>
-            </main>
+  useEffect(() => {
+    checkAuthenticated();
+  }, []);
 
-            {/* <Footer /> */}
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const setAuth = (boolean) => {
+    setIsAuthenticated(boolean);
+  };
+
+  return (
+    <Fragment>
+      <Router>
+        <div className="container">
+          <Switch>
+            <Route
+              exact
+              path="/login"
+              render={(props) =>
+                !isAuthenticated ? (
+                  <Login {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/dashboard" />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/register"
+              render={(props) =>
+                !isAuthenticated ? (
+                  <Register {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/login" />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/dashboard"
+              render={(props) =>
+                isAuthenticated ? (
+                  <Dashboard {...props} setAuth={setAuth} />
+                ) : (
+                  <Redirect to="/login" />
+                )
+              }
+            />
+          </Switch>
         </div>
-    );
+      </Router>
+    </Fragment>
+  );
 }
 
 export default App;
---
+++ client/src/components/Login.js
@@ -0,0 +1,145 @@
+import React, { Fragment, useState } from "react";
+// import { toast } from "react-toastify";
+import { Link } from "react-router-dom";
+// import '../styles/Login.css';
+
+const Login = ({ setAuth }) => {
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = inputs;
+
+  const onChange = (e) =>
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { email, password };
+      const response = await fetch("http://localhost:3000/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+
+      const parsedResponse = await response.json();
+
+      if (parsedResponse.token) {
+        localStorage.setItem("token", parsedResponse.token);
+        setAuth(true);
+        // toast.success("Logged in Successfully");
+      } else {
+        setAuth(false);
+        // toast.error(parseRes);
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+  return (
+    <Fragment>
+      <h1 className="mt-5 text-center"> Login </h1>
+      <form onSubmit={onSubmitForm}>
+        <input
+          type="text"
+          name="email"
+          placeholder="Type your email"
+          className="form-control my-3"
+          value={email}
+          onChange={(e) => onChange(e)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Type your password"
+          className="form-control my-3"
+          value={password}
+          onChange={(e) => onChange(e)}
+        />
+        <button className="btn btn-success btn-block">Login</button>
+      </form>
+      <Link to="/register">No account? No problem! Sign up here</Link>
+    </Fragment>
+  );
+};
+
+export default Login;
+
+// const Login = ({ setAuth }) => {
+//   const [inputs, setInputs] = useState({
+//     email: "",
+//     password: "",
+//   });
+
+//   const { email, password } = inputs;
+
+//   const onChange = (e) =>
+//     setInputs({ ...inputs, [e.target.name]: e.target.value });
+
+//   const onSubmitForm = async (e) => {
+//     e.preventDefault();
+//     try {
+//       const body = { email, password };
+//       const response = await fetch("http://localhost:8000/auth/login", {
+//         method: "POST",
+//         headers: {
+//           "Content-type": "application/json",
+//         },
+//         body: JSON.stringify(body),
+//       });
+
+//       const parseRes = await response.json();
+
+//       if (parseRes.token) {
+//         localStorage.setItem("token", parseRes.token);
+//         setAuth(true);
+//         toast.success("Logged in Successfully");
+//       } else {
+//         setAuth(false);
+//         toast.error(parseRes);
+//       }
+//     } catch (err) {
+//       console.error(err.message);
+//     }
+//   };
+
+//   return (
+//     <>
+//       <br />
+//       <Link to="/" style={{ textDecoration: "none" }}>
+//         <h1 className="headerLogo">HabiTrack</h1>
+//       </Link>
+//       <hr id="loginHr" />
+//       <form id="loginPageForm" onSubmit={onSubmitForm}>
+//         <label htmlFor="email">Email</label>
+//         <input
+//           id="email"
+//           type="text"
+//           name="email"
+//           value={email}
+//           placeholder="Type your email"
+//           onChange={(e) => onChange(e)}
+//           autoFocus
+//           autoComplete="off"
+//         />
+//         <label htmlFor="password">Password</label>
+//         <input
+//           id="password"
+//           type="password"
+//           name="password"
+//           value={password}
+//           placeholder="Type your password"
+//           onChange={(e) => onChange(e)}
+//         />
+//         <button className="submitButtons">Login</button>
+//       </form>
+//       <Link to="/register">Not registered? Sign up here</Link>
+//     </>
+//   );
+// };
+
+// export default Login;
