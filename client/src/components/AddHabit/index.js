ad75acebbdc820f8d55e955857231e33d16e4699
Stylianos Thomas <stylianos.thomas@gmail.com>
2021-01-14 23:37:15 +0000

    checkboxes are now working properly!

--- client/src/components/AddHabit/index.js
+++ client/src/components/AddHabit/index.js
@@ -3,33 +3,73 @@ import React from 'react';
 class AddHabit extends React.Component {
     state = {
         user_id : this.props.user_id,
-        habitData: {
-            title: null,
-            description: null,
-            monday: false,
-            tuesday: false,
-            wednesday: false,
-            thursday: false,
-            friday: false,
-            saturday: false,
-            sunday: false,
-        }
+        title: "",
+        description: "",
+        monday: false,
+        tuesday: false,
+        wednesday: false,
+        thursday: false,
+        friday: false,
+        saturday: false,
+        sunday: false,
     }
 
     handleCheck = e => {
-        let day = e.target.name
-        this.state.habitData.day === false ?
-        this.setState({ day: true }) :
-        this.setState({ day: false })
+        switch ( e.target.name) {
+            case "monday" :
+                this.setState({monday: !this.state.monday});
+                break;
+            case "tuesday" :
+                this.setState({tuesday: !this.state.tuesday});
+                break;
+            case "wednesday" :
+                this.setState({wednesday: !this.state.wednesday});
+                break;
+            case "thursday" :
+                this.setState({thursday: !this.state.thursday});
+                break;
+            case "friday" :
+                this.setState({friday: !this.state.friday});
+                break;
+            case "saturday" :
+                this.setState({saturday: !this.state.saturday});
+                break;
+            case "sunday" :
+                this.setState({sunday: !this.state.sunday});
+                break;
+            default :
+                console.log("this should never appear");
+        }
+    }
+
+    handleTitle = e => {
+        const inputTitle = e.target.value
+        this.setState({ title: inputTitle })
     }
 
     handleDescription = e => {
         const inputDescription = e.target.value
-        this.setState({ title: inputDescription })
+        this.setState({ description: inputDescription })
     }
 
     handleSubmit = e => {
         e.preventDefault();
+        const options = {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(this.state.habitData)  // does this need to be an object?
+        }
+
+        fetch('http://localhost:3000/dashboard/create', options)    //route might be wrong
+            .then(r => r.json())
+            .then(resp => {
+                console.log('resp is', resp)
+                const {id, err} = resp;
+                if(err) { throw Error(err) }
+                else {history.push("/dashboard")}   //how to change history
+            })
+            // .catch(err => console.log('oops', err.message))
+            .catch(err => setAlert(err.message))
     }
 
     render() {
@@ -37,28 +77,27 @@ class AddHabit extends React.Component {
             <div id='add-habit-form'>
                 <h1> Add Habit! </h1>
                 <form>
-                    <input type='text' placeholder='Habit Name' onChange={this.handleTitle}></input><br></br>
-                    <textarea name='description' rows='4' cols='50' placeholder='Description' onClick={this.handleDescription}></textarea><br></br>
-                    <input type="checkbox" id="monday" name="monday" value="monday" onClick={this.handleCheck}></input>
-                    <label for="monday">    Monday </label><br></br>
-                    <input type="checkbox" id="tuesday" name="tuesday" value="tueday" onClick={this.handleCheck}></input>
-                    <label for="tuesday">   Tuesday </label><br></br>
-                    <input type="checkbox" id="wednesday" name="wednesday" value="wednesday" onClick={this.handleCheck}></input>
-                    <label for="wednesday">     Wednesday</label><br></br>
-                    <input type="checkbox" id="thursday" name="thursday" value="thursday" onClick={this.handleCheck}></input>
-                    <label for="thursday">  Thursday </label><br></br>
-                    <input type="checkbox" id="friday" name="friday" value="friday" onClick={this.handleCheck}></input>
-                    <label for="friday">    Friday </label><br></br>
-                    <input type="checkbox" id="saturday" name="saturday" value="saturday" onClick={this.handleCheck}></input>
-                    <label for="saturday">  Saturday </label><br></br>
-                    <input type="checkbox" id="sunday" name="sunday" value="sunday" onClick={this.handleCheck}></input>
-                    <label for="sunday">    Sunday </label><br></br>
-                    <input type='submit' onSubmit={this.handleSubmit}></input>
+                    <input type='text' placeholder='Habit Name' value={this.state.title} onChange={this.handleTitle} /><br/>
+                    <textarea name='description' rows='4' cols='50' placeholder='Description' value={this.state.description} onChange={this.handleDescription} /><br/>
+                    <input type="checkbox" id="monday" name="monday" value="monday" checked={this.state.monday} onChange={this.handleCheck} />
+                    <label htmlFor="monday"> Monday </label><br/>
+                    <input type="checkbox" id="tuesday" name="tuesday" value="tuesday" checked={this.state.tuesday} onChange={this.handleCheck} />
+                    <label htmlFor="tuesday"> Tuesday </label><br/>
+                    <input type="checkbox" id="wednesday" name="wednesday" value="wednesday" checked={this.state.wednesday} onChange={this.handleCheck} />
+                    <label htmlFor="wednesday"> Wednesday</label><br/>
+                    <input type="checkbox" id="thursday" name="thursday" value="thursday" checked={this.state.thursday} onChange={this.handleCheck} />
+                    <label htmlFor="thursday"> Thursday </label><br/>
+                    <input type="checkbox" id="friday" name="friday" value="friday" checked={this.state.friday} onChange={this.handleCheck} />
+                    <label htmlFor="friday"> Friday </label><br/>
+                    <input type="checkbox" id="saturday" name="saturday" value="saturday" checked={this.state.saturday} onChange={this.handleCheck} />
+                    <label htmlFor="saturday"> Saturday </label><br/>
+                    <input type="checkbox" id="sunday" name="sunday" value="sunday" checked={this.state.sunday} onChange={this.handleCheck} />
+                    <label htmlFor="sunday"> Sunday </label><br/>
+                    <input type='submit' onSubmit={this.handleSubmit} />
                 </form>
             </div>
         )
     }
-
 }
 
 export default AddHabit;
